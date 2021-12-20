14f8a8cca244910f98a9354bbb0d58685b3ab4d7
haidagn <96415975+undefinednulla@users.noreply.github.com>
2021-12-20 23:58:36 +0700

    dark, light mode config

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,36 @@
-console.log("Hello Word")
+
+
+const app = {
+    modeColor: function () {
+        document.getElementById('dark').addEventListener('click', () => {
+            document.documentElement.style.setProperty("--primarycolor", "")
+            document.documentElement.style.setProperty("--textlight", "")
+            document.documentElement.style.setProperty("--textdark", "")
+            document.documentElement.style.setProperty("--graymedium", "")
+            document.documentElement.style.setProperty("--graylight", "")
+            console.log('modedard')
+        })
+        document.getElementById('light').addEventListener('click', () => {
+            document.documentElement.style.setProperty("--primarycolor", "#FFFFFF")
+            document.documentElement.style.setProperty("--textlight", "#22252d")
+            document.documentElement.style.setProperty("--textdark", "#F1F1F1")
+            document.documentElement.style.setProperty("--graymedium", "#F9F9F9")
+            document.documentElement.style.setProperty("--graylight", "#F7F7F7")
+            console.log('modedar')
+        })
+    },
+    handle: function () {
+
+    },
+    render: function () {
+        //code
+    },
+    start: function () {
+        this.handle()
+        this.modeColor()
+        this.render()
+    }
+}
+
+app.start()
+
